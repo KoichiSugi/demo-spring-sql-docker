e37f0b2da907828cb945f28c8c25efe5e1b97857
KoichiSugi <43499213+KoichiSugi@users.noreply.github.com>
2021-02-27 21:29:50 +1100

    commit#2
    little Modification

--- employee-jdbc/src/main/java/com/java/javainuse/configuration/JpaConfig.java
+++ employee-jdbc/src/main/java/com/java/javainuse/configuration/JpaConfig.java
@@ -13,7 +13,7 @@ public class JpaConfig {
 	public DataSource getDataSource() {
 		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
 		//dataSourceBuilder.url("jdbc:mysql://localhost/test?createDatabaseIfNotExist=true&autoReconnect=true&useSSL=false");
-		dataSourceBuilder.url("jdbc:mysql://mysqldb/test?createDatabaseIfNotExist=true&autoReconnect=true&useSSL=false");
+		dataSourceBuilder.url("jdbc:mysql://mysqldb/test");
 		dataSourceBuilder.username("root");
 		dataSourceBuilder.password("root");
 		return dataSourceBuilder.build();
